87bd24395f19dca460d4b0b72b2e5459d82afa8e
StanleeO <stanlee.okwii@gmail.com>
2024-11-07 01:01:42 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+const mongoose = require('mongoose');
+require('dotenv/config');
+
+const bodyParser = require('body-parser');
+const postsRoute = require('./routes/posts');
+
+app.use(bodyParser.json());
+app.use('/posts', postsRoute);
+
+app.get('/', (req, res) => {
+    res.send('HomePage');
+});
+
+mongoose.connect(process.env.DB_CONNECTOR).then(() => {
+    console.log('Your mongoDB connector is on... Atlas');
+})
+
+app.listen(3000, ()=>{
+    console.log('Server is up and running on: http://localhost:3000');
+})
